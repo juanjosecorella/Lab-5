58508b3ee576a1d20aea467ee7f01a615c10f4ca
juanjosecorella <juanjosecorella2004@gmail.com>
2025-10-04 10:47:46 -0600

    mocha update y rate limiting

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var rateLimit = require("express-rate-limit")
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
 // root: presentar html
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
